58d5e4020a991e913fd615c4f6b6f8018b155e18
OpenShift guest <arnoldthompson11@yahoo.com>
2014-04-23 10:12:07 -0400

    homework 4

--- Library.java
+++ Library.java
@@ -1,6 +1,63 @@
-public class Library {
-    // Add the missing implementation to this class
+import java.util.ArrayList;
 
+public class Library {
+    String address;
+    ArrayList<Book> books;
+    
+    public Library (String address){
+        this.address = address;
+        books = new ArrayList<Book>();
+    }    
+    
+    public void addBook(Book book_obj){
+        books.add(book_obj);
+    }
+
+    public static void printOpeningHours(){
+        System.out.println("Libraries are open daily form 9am to 5pm");
+    }
+    
+    public void printAddress(){
+        System.out.println(address);
+    }
+    
+    public void borrowBook(String book_name){
+        boolean available_flag = false;
+        for (Book book: books){
+            if ( book.title == book_name && !book.isBorrowed() ){
+                book.rented();
+                available_flag = true;
+                System.out.println("Successfully borrow: " + book.title );
+            }
+        }
+        
+        if ( !available_flag ){
+            System.out.println("Sorry, this book is not available");
+        }
+    }
+    
+    public void returnBook(String book_name){
+        for (Book book: books){
+            if (book.title == book_name){
+                book.returned();
+                System.out.println("Successfully return: " + book.title );
+            }
+        }
+    }
+    
+    public void printAvailableBooks(){
+        boolean available_flag = false;
+        for (Book book: books){
+            if ( !book.isBorrowed() ){
+                System.out.println(book.title);
+                available_flag = true;
+            }
+        }
+        if ( !available_flag ){
+            System.out.println("Sorry, no book is available");
+        }
+    }
+    
     public static void main(String[] args) {
         // Create two libraries
         Library firstLibrary = new Library("10 Main St.");
